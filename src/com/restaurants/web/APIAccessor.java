233f1a1d889402e28dd7e27271bf06ad0664c1d9
Lewis Trowbridge <lewbob9927@gmail.com>
2021-03-14 17:17:55 +0000

    Added implementation of getAllRestaurants

--- src/com/restaurants/web/APIAccessor.java
+++ src/com/restaurants/web/APIAccessor.java
@@ -1,12 +1,41 @@
 package com.restaurants.web;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.restaurants.models.Restaurant;
+import com.restaurants.models.Restaurants;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 
 public class APIAccessor implements IAPIAccessor {
+
+    final private HttpClient client;
+
+    public APIAccessor() {
+        client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .build();
+    }
+
     @Override
     public ArrayList<Restaurant> getAllRestaurants() {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://intelligent-social-robots-ws.com/restaurant-data.json"))
+                .build();
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            ObjectMapper mapper = new ObjectMapper();
+            Restaurants restaurants = mapper.readValue(response.body(), Restaurants.class);
+            return restaurants.restaurants;
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
