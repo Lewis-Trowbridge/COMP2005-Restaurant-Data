b44379f910093b62a4033f70f824a8f119db9431
Lewis Trowbridge <lewbob9927@gmail.com>
2021-03-15 12:35:58 +0000

    Added mock object for API accessor for testing purposes and test data

---
+++ tests/com/restaurants/web/MockAPIAccessor.java
@@ -0,0 +1,29 @@
+package com.restaurants.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.restaurants.models.Restaurant;
+import com.restaurants.models.Restaurants;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MockAPIAccessor implements IAPIAccessor {
+
+    ObjectMapper mapper;
+
+    public MockAPIAccessor() {
+        mapper = new ObjectMapper();
+    }
+
+    @Override
+    public ArrayList<Restaurant> getAllRestaurants() {
+        try {
+            Restaurants restaurants = mapper.readValue(new File("./testresources/restaurant-data.json"), Restaurants.class);
+            return restaurants.restaurants;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
