3e83f99551756da8680ba0d03bc6591f401971e5
Lewis Trowbridge <lewbob9927@gmail.com>
2021-04-13 19:59:42 +0100

    Added implementation of filter by day of week

--- src/com/restaurants/filters/RestaurantFilters.java
+++ src/com/restaurants/filters/RestaurantFilters.java
@@ -1,5 +1,6 @@
 package com.restaurants.filters;
 
+import com.restaurants.models.OpeningHours;
 import com.restaurants.models.Restaurant;
 
 import java.util.ArrayList;
@@ -34,8 +35,54 @@ public class RestaurantFilters {
         return null;
     }
 
-    public ArrayList<Restaurant> filterRestaurantsByDayOfWeek(ArrayList<Restaurant> restaurants, String dayOfWeek){
-        return null;
+    public ArrayList<Restaurant> filterRestaurantsByDayOfWeek(ArrayList<Restaurant> restaurants, String dayOfWeek) throws IllegalArgumentException{
+        ArrayList<Restaurant> restaurantsCopy = new ArrayList<>();
+
+        for (Restaurant currentRestaurant: restaurants) {
+            OpeningHours hours = currentRestaurant.getOpeningHours();
+            switch (dayOfWeek){
+                case "Monday":
+                case "Mon":
+                    if (hours.getMondayOpen()[0] != null && hours.getMondayClose()[0] != null){
+                        restaurantsCopy.add(currentRestaurant);
+                    }
+                    break;
+                case "Tuesday":
+                case "Tue":
+                    if (hours.getTuesdayOpen()[0] != null && hours.getTuesdayClose()[0] != null){
+                        restaurantsCopy.add(currentRestaurant);
+                    }
+                case "Wednesday":
+                case "Wed":
+                    if (hours.getWednesdayOpen()[0] != null && hours.getWednesdayClose()[0] != null){
+                        restaurantsCopy.add(currentRestaurant);
+                    }
+                case "Thursday":
+                case "Thurs":
+                    if (hours.getThursdayOpen()[0] != null && hours.getThursdayClose()[0] != null){
+                        restaurantsCopy.add(currentRestaurant);
+                    }
+                case "Friday":
+                case "Fri":
+                    if (hours.getFridayOpen()[0] != null && hours.getFridayClose()[0] != null){
+                        restaurantsCopy.add(currentRestaurant);
+                    }
+                case "Saturday":
+                case "Sat":
+                    if (hours.getSaturdayOpen()[0] != null && hours.getSaturdayClose()[0] != null){
+                        restaurantsCopy.add(currentRestaurant);
+                    }
+                case "Sunday":
+                case "Sun":
+                    if (hours.getSundayOpen()[0] != null && hours.getSundayClose()[0] != null){
+                        restaurantsCopy.add(currentRestaurant);
+                    }
+                default:
+                    throw new IllegalArgumentException();
+            }
+        }
+
+        return restaurantsCopy;
     }
 
 }
