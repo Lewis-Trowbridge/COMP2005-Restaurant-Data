e4182a3c9125314a2d41513453e3a5779319ce44
Lewis Trowbridge <lewbob9927@gmail.com>
2021-04-17 13:10:13 +0100

    Added custom constructor for LatLngCoordinates

--- src/com/restaurants/models/LatLngCoordinates.java
+++ src/com/restaurants/models/LatLngCoordinates.java
@@ -1,10 +1,21 @@
 package com.restaurants.models;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class LatLngCoordinates {
     private double latitude;
     private double longitude;
+
+    @JsonCreator
+    public LatLngCoordinates(){
+
+    }
+
+    public LatLngCoordinates(double latitude, double longitude){
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 
     @JsonProperty("lat")
     public double getLatitude() {
