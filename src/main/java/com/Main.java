6f40e7faabedd4222641b089c02f7f6de8fd66dd
Lewis Trowbridge <lewbob9927@gmail.com>
2021-04-29 13:48:27 +0100

    Removed unnecessary main code

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -9,11 +9,5 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        APIAccessor help = new APIAccessor();
-        ArrayList<Restaurant> restaurants = help.getAllRestaurants();
-        RestaurantFilters filters = new RestaurantFilters();
-
-        filters.orderRestaurantsByDistanceFromHotel(restaurants);
     }
 }
